e23037fac0cd4119a41bbe3ef6cf602ad00d9367
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-08-12 19:28:46 +0200

    add post apps and your models for post

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'posts',
+
+    # my package 
+    "django_bootstrap5",
+    "taggit",
 ]
 
 MIDDLEWARE = [

407daee4aae89fa5011b2b9ee5fd2fcebb34eb25
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-08-17 13:48:39 +0200

    add brand detail

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import Product_Detail,Product_List,add_review
+from .views import Product_Detail,Product_List,add_review,Brand_List,Brand_Detail
 
 urlpatterns = [
     path('',Product_List.as_view()),
+    path('brands/<slug:slug>',Brand_Detail.as_view()),
+
+    path('brands',Brand_List.as_view()),
+
     path('add_review/<slug:slug>',add_review),
     path('<slug:slug>',Product_Detail.as_view()),
 
--- products/views.py
+++ products/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render,redirect
 from .models import Product,Brand,Review,ProductImage
 from django.views.generic import ListView,DetailView
 
+from django.db.models.aggregates import Count
+
 class Product_List(ListView):
     model=Product
     template_name='products/product_list.html'
@@ -36,5 +38,42 @@ def add_review(request,slug):
         review=review
     )
     return redirect(f'/products/{slug}')
+
+class Brand_List(ListView):
+    model=Brand
+    template_name='products/brands_list.html'
+    paginate_by=25
+
+    queryset=Brand.objects.annotate(product_count=Count('product_brand'))
+
+# class Brand_Detail(DetailView):
+#     model=Brand
+#     template_name='products/brand_detail.html'
+
+#     def get_context_data(self, **kwargs) :
+#         context = super().get_context_data(**kwargs)
+#         context["related"] = Product.objects.filter(brand=self.get_object()) 
+#         return context
+    
+class Brand_Detail(ListView):
+    model=Product
+    template_name='products/brand_detail.html'
+    paginate_by=5
+
+    def get_queryset(self):
+        brand=Brand.objects.get(slug=self.kwargs['slug'])
+        queryset= super().get_queryset().filter(brand=brand)
+        return queryset
+    
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        context["brand"] = Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count=Count('product_brand'))[0]
+        return context
+    
+    
+
+   
+    
+    
 
     
